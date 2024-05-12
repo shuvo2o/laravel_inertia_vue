5294febc221bce68613c30aa48efc4e5603b37d8
prismtechbd3 <prismtechb@gmail.com>
2024-05-12 21:36:13 +0600

    laravel_inertia_vue started

---
+++ resources/js/Pages/Test.vue
@@ -0,0 +1,9 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+</script>
+<template>
+    <AuthenticatedLayout>
+        <h1>This is Test</h1>    
+    </AuthenticatedLayout>
+    
+</template>
